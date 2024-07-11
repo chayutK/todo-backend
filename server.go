cf6213d0d24419a5c614b2862cd866ac65e46352
chayutK <chayut.k@arise.tech>
2024-07-11 14:12:42 +0700

    Gracefully shutdown

--- server.go
+++ server.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"context"
 	"database/sql"
+	"errors"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	repository "github.com/chayutK/todo-backend/repository"
 	"github.com/gin-gonic/gin"
@@ -11,14 +19,42 @@ import (
 var db *sql.DB
 
 func main() {
-
-	db = repository.DatabaseSync()
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
 
 	r := gin.Default()
-
 	r.GET("/", helloWorldHandler)
 
-	r.Run()
+	srv := http.Server{
+		Addr:    ":" + os.Getenv("PORT"),
+		Handler: r,
+	}
+
+	closeChannel := make(chan struct{})
+	go func() {
+		<-ctx.Done()
+		fmt.Println("Server is shutting down.....")
+
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+		defer cancel()
+
+		if err := srv.Shutdown(ctx); err != nil {
+			if !errors.Is(err, http.ErrServerClosed) {
+				log.Println(err.Error())
+			}
+		}
+
+		close(closeChannel)
+
+	}()
+
+	db = repository.DatabaseSync()
+	if err := srv.ListenAndServe(); err != nil {
+		log.Println(err.Error())
+	}
+
+	<-closeChannel
+	fmt.Println("Server is closed")
 
 }
 
